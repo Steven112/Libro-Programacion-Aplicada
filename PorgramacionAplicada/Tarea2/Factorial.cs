12f41560a460edc3c295b5d4cc1fd3712a38ec24
Steven112 <54567861+Steven112@users.noreply.github.com>
2019-09-02 23:23:25 -0400

    Tarea1

---
+++ PorgramacionAplicada/Tarea2/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PorgramacionAplicada.Tarea2
+{
+    class Factorial
+    {
+        public void factor()
+        {
+
+            int n, i,factorial;
+            Console.WriteLine(" Dijite el valor del numero");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = n; i=1 ; i--)
+            {
+                factorial=factorial*i
+                Console.WriteLine("Factorial de {0}={1}", n,factorial);
+            }
+
+        }
+    }
+}
---
+++ PorgramacionAplicada/Tarea2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PorgramacionAplicada.Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+
+        }
+    }
+}
